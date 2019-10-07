9cb4a56b836e5cc9b14106608c723a723b157b48
ehummel7 <44881508+ehummel7@users.noreply.github.com>
2019-10-07 16:00:12 -0400

    Add files via upload

---
+++ CharacterAttack.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAttack : MonoBehaviour
+{
+	public float range = 3.0f;    //Attack range
+	public int knives = 1;   //One throwable knife
+
+	public Transform attackPoint;    //Point knife is swung
+	public GameObject hitParticles; //Attack impact
+
+	public float slashRate = 1.0f;   //Speed player can slash while holding down m1
+	float slashTimer;    //Time counter for slash rate
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		if (Input.GetButton("Fire1"))
+		{
+			if (knives > 0)
+				Fire(); //Attack when button is held
+		}
+
+		if (slashTimer < slashRate)
+			slashTimer += Time.deltaTime;    //Add to time counter
+	}
+
+	void FixedUpdate()
+	{
+
+	}
+
+	private void Fire()
+	{
+		if (slashTimer < slashRate) return;
+		Debug.Log("Fire!");
+
+
+		RaycastHit hit;
+
+		if (Physics.Raycast(attackPoint.position, attackPoint.transform.forward, out hit, range))
+		{
+			Debug.Log("hit");
+
+			//GameObject hitParticleEffect = Instantiate(hitParticles, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
+
+			//Destroy(hitParticleEffect, 1f);
+		}
+
+		slashTimer = 0.0f;   //Reset fire timer
+	}
+}
